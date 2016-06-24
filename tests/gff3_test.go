e792f47b7cac71f3150d3fdd55eba53132c9ca87
Karl Kroll <kwkroll.32@gmail.com>
2016-06-24 12:54:13 -0400

    more thorough test of record parse, implementing deep comparison

--- tests/gff3_test.go
+++ tests/gff3_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"reflect"
 	"strings"
 	"testing"
 
@@ -12,11 +13,41 @@ var fakeGff3Line = "chr1\tHAVANA\tgene\t11869\t14409\t.\t+\t.\tID=ENSG0000022397
 
 func TestSimpleParseLineWithReader(t *testing.T) {
 	myReader := gff3.NewReader(strings.NewReader(fakeGff3Line))
-	if myRecord, err := myReader.Read(); err != nil {
+	myRecord, err := myReader.Read()
+	if err != nil {
 		t.Errorf("record was not correctly parsed and returned an error")
-	} else if !myRecord.Complete {
+	}
+	// file opened fine, but is the content correct?
+	var fakeGff3Solution = gff3.Record{
+		Complete:    true,
+		SeqidField:  "chr1",
+		SourceField: "HAVANA",
+		TypeField:   "gene",
+		StartField:  11869,
+		EndField:    14409,
+		ScoreField:  0,
+		StrandField: '+',
+		PhaseField:  0,
+		AttributesField: map[string]string{
+			"ID":          "ENSG00000223972.5",
+			"gene_id":     "ENSG00000223972.5",
+			"gene_type":   "transcribed_unprocessed_pseudogene",
+			"gene_status": "KNOWN",
+			"gene_name":   "DDX11L1",
+			"level":       "2",
+			"havana_gene": "OTTHUMG00000000961.2",
+		},
+	}
+	// deep equal will recursively compare all attributes and values of the structs
+	// a simple equal "==" doesn't suffice because keys in the attributes map may be reordered
+	if !reflect.DeepEqual(myRecord, &fakeGff3Solution) {
 		t.Errorf("record was not correctly parsed, but did not throw an error")
 	}
+	fakeGff3Solution.StartField = 1
+	if reflect.DeepEqual(myRecord, &fakeGff3Solution) {
+		t.Errorf("records are different and should not be equal")
+	}
+
 }
 
 func TestParseTwoLinesWithReader(t *testing.T) {
